daa8fbab5950ddb2948fc0dd947054a948173509
Kage Kowalski <kagekowalski@gmail.com>
2024-12-04 22:23:30 -0600

    2024 q4 p1 finished and verified

--- 2024/q4/p1.py
+++ 2024/q4/p1.py
@@ -7,11 +7,10 @@ def complete_word(parsed_data: list[list[str]], word: str, root_coords: (int,int
     if len(word) == letter:
         return True
     letter_coords = (root_coords[0] + (direction[0] * letter), root_coords[1] + (direction[1] * letter))
-    try:
-        if word[letter:letter+1] == parsed_data[letter_coords[0]][letter_coords[1]]:
-            return complete_word(parsed_data, word, root_coords, letter+1, direction)
-    except IndexError:
+    if letter_coords[0] < 0 or letter_coords[1] < 0 or letter_coords[0] >= len(parsed_data) or letter_coords[1] >= len(parsed_data[letter_coords[0]]):
         return False
+    if word[letter:letter+1] == parsed_data[letter_coords[0]][letter_coords[1]]:
+        return complete_word(parsed_data, word, root_coords, letter+1, direction)
     return False
 
 
@@ -19,11 +18,9 @@ def find_first_neighbors(parsed_data: list[list[str]], word: str, root_coords: (
     first_neighbors = []
     for ri in range(-1, 2):
         for ci in range(-1, 2):
-            try:
-                if parsed_data[root_coords[0] + ri][root_coords[1] + ci] == word[1:2] and ri != 0 and ci != 0:
-                    first_neighbors.append((ri,ci))
-            except IndexError:
-                pass
+            if root_coords[0] + ri > -1 and root_coords[1] + ci > -1 and root_coords[0] + ri < len(parsed_data) and root_coords[1] + ci < len(parsed_data[root_coords[0]]) and parsed_data[root_coords[0] + ri][root_coords[1] + ci] == word[1:2] and (ri != 0 or ci != 0):
+                print(f'Char 2: {parsed_data[root_coords[0] + ri][root_coords[1] + ci]}, Distance: {ri},{ci}')
+                first_neighbors.append((ri,ci))
     return first_neighbors
 
 
@@ -32,9 +29,10 @@ def count_word(parsed_data: list[list[str]], word: str) -> int:
     for r in range(len(parsed_data)):
         for c in range(len(parsed_data[r])):
             if parsed_data[r][c] == word[:1]:
+                print(f'Char 1: {parsed_data[r][c]}, Coords: {r},{c}') # Correctly finding root character
                 first_neighbors = find_first_neighbors(parsed_data, word, (r,c))
                 for f_n in first_neighbors:
-                    if complete_word(parsed_data, word, (r,c), 2, (f_n[0] - r, f_n[1] - c)):
+                    if complete_word(parsed_data, word, (r,c), 2, (f_n[0], f_n[1])):
                         words_found = words_found + 1
     return words_found
 
@@ -49,4 +47,4 @@ def main(in_file, out_file, word):
 
 
 if __name__ == '__main__':
-    main('simple_in.txt', 'p1_out.txt', 'XMAS')
+    main('in.txt', 'p1_out.txt', 'XMAS')
