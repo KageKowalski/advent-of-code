13922b846bd5bc1edb0d2bf8ae4b93742bdeff23
Kage Kowalski <kagekowalski@gmail.com>
2024-12-05 21:22:20 -0600

    2024 q3 p2 finished and verified

--- 2024/q3/p2.py
+++ 2024/q3/p2.py
@@ -1,32 +1,25 @@
 import re
-from p1 import sum_muls
-
-
-def format_data(in_file: str):
-    formatted_data = ''
-    with open(in_file, 'r') as f:
-        for line in f:
-            if not line.startswith('do()'):
-                formatted_data = formatted_data + 'do()'
-            formatted_data = formatted_data + line[:len(line)-1]
-            if not line.endswith('don\'t()'):
-                formatted_data = formatted_data + 'don\'t()' + '\n'
-    return formatted_data
-
-
-def parse_data(formatted_data: str):
-    parsed_data = []
-    matches = re.finditer(r'^do\(\).*don\'t\(\)', formatted_data)
-    for match in matches:
-        print(match.group())
-        for mul in re.findall(r'mul\(\d+,\d+\)', match.group()):
-            parsed_data.append(mul)
-    return parsed_data
 
 
 def main(in_file: str, out_file: str):
+    total = 0
+    pattern = r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)'
+
+    with open(in_file, 'r') as f:
+        parsed_data = re.findall(pattern, f.read())
+
+    recording = True
+    for instr in parsed_data:
+        if re.match(r'do\(\)', instr):
+            recording = True
+        elif re.match(r'don\'t\(\)', instr):
+            recording = False
+        elif recording:
+            nums = re.findall(r'\d+', instr)
+            total = total + (int(nums[0]) * int(nums[1]))
+
     with open(out_file, 'w') as f:
-        f.write(str(sum_muls(parse_data(format_data(in_file)))))
+        f.write(str(total))
 
 
 if __name__ == '__main__':
